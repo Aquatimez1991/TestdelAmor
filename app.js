93fd39df50948cc1543cf79ab1b867a965c577f9
Aquatimez1991 <esriaden0.1@gmail.com>
2025-02-02 23:14:55 -0500

    Agrega Funciones de dificultad

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         button.style.top = `${y}px`;
     }
 
+    function makeButtonHarderToClick(event) {
+        const rect = button.getBoundingClientRect();
+        const mouseDistance = Math.sqrt(Math.pow(event.clientX - rect.left - rect.width / 2, 2) +
+                                        Math.pow(event.clientY - rect.top - rect.height / 2, 2));
+        if (mouseDistance < 1) {  // Aumenta la distancia para hacerlo más fácil
+            moveButton();
+        }
+    }
+
     function redirectToPage() {
         window.location.href = 'pagmeme1.html';
     }
 
     button.addEventListener('mouseover', moveButton);
+    button.addEventListener('mousemove', makeButtonHarderToClick);
+    button.addEventListener('touchstart', moveButton);  // Para dispositivos táctiles
+    button.addEventListener('touchmove', makeButtonHarderToClick);  // Para dispositivos táctiles
     button.addEventListener('click', redirectToPage);
 });
